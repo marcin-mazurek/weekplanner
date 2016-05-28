38f6f38eb2c58c4d27446e2cb051a6dd86f4b56e
Marcin Mazurek <marcin@mazurek.pro>
2016-05-28 18:38:58 +0100

    UI prototype - clean up Card component (view mode)

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,9 +3,6 @@ import styles from './card.scss';
 import IconButton from 'material-ui/IconButton';
 import ClockIcon from 'material-ui/svg-icons/device/access-time';
 import DoneIcon from 'material-ui/svg-icons/action/done';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
-import ColorIcon from 'material-ui/svg-icons/action/invert-colors';
-import RepeatIcon from 'material-ui/svg-icons/av/repeat';
 import Paper from 'material-ui/Paper';
 import Checkbox from 'material-ui/Checkbox';
 import cx from 'classnames';
@@ -28,10 +25,6 @@ export default class Card extends Component {
     }
   }
 
-  isEditMode() {
-    return this.props.variant === 'editModeModal';
-  }
-
   renderTime() {
     if (this.props.time) {
       return (
@@ -43,20 +36,9 @@ export default class Card extends Component {
     }
   }
 
-  renderDoneHoverButton() {
-    if (!this.isEditMode()) {
-      // TODO: provide event handler
-      return (
-        <IconButton className={styles.doneButton} onTouchTap={event => event.stopPropagation()}>
-          <DoneIcon className={styles.doneIcon} />
-        </IconButton>
-      );
-    }
-  }
-
   renderDescription() {
     if (this.props.description) {
-      return <p contentEditable={this.isEditMode()} className={styles.description}>{this.props.description}</p>;
+      return <p className={styles.description}>{this.props.description}</p>;
     }
   }
 
@@ -80,42 +62,19 @@ export default class Card extends Component {
     );
   }
 
-  renderActions() {
-    if (this.isEditMode()) {
-      return (
-        <div className={styles.actions}>
-          <IconButton tooltip="Add reminder" onTouchTap={event => event.stopPropagation()}>
-            <ClockIcon />
-          </IconButton>
-          <IconButton tooltip="Change color" onTouchTap={event => event.stopPropagation()}>
-            <ColorIcon />
-          </IconButton>
-          <IconButton tooltip="Set repeat rules" onTouchTap={event => event.stopPropagation()}>
-            <RepeatIcon />
-          </IconButton>
-          <IconButton tooltip="Delete" onTouchTap={event => event.stopPropagation()}>
-            <DeleteIcon />
-          </IconButton>
-          <IconButton tooltip="Mark as done" onTouchTap={event => event.stopPropagation()}>
-            <DoneIcon />
-          </IconButton>
-        </div>
-      )
-    }
-  }
-
   render() {
     return (
-      <Paper zDepth={this.isEditMode() ? 5 : 1}
-        className={cx(styles.card, styles[this.props.variant])}
+      <Paper className={cx(styles.card, styles[this.props.variant])}
         style={this.getCardStyles()}
         onTouchTap={this.handleClick}>
-        {this.renderDoneHoverButton()}
+        { /* TODO: provide event handler */ }
+        <IconButton className={styles.doneButton} onTouchTap={event => event.stopPropagation()}>
+          <DoneIcon className={styles.doneIcon} />
+        </IconButton>
         {this.renderTime()}
-        <h3 className={styles.title} contentEditable={this.isEditMode()}>{this.props.title}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
         {this.renderDescription()}
         {this.renderChecklist()}
-        {this.renderActions()}
       </Paper>
     );
   }
@@ -123,7 +82,7 @@ export default class Card extends Component {
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  variant: PropTypes.oneOf(['plain', 'editModeModal', 'editModeOriginal']).isRequired,
+  variant: PropTypes.oneOf(['plain', 'edited']).isRequired,
   onTouchTap: PropTypes.func,
   description: PropTypes.string,
   time: PropTypes.string,
